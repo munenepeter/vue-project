2dad1b64508ac2c67e1e93ae229c8a721e757599
munenepeter <peternjega7@gmail.com>
2023-10-19 01:06:53 +0300

    finished with backend

--- app/Http/Controllers/Api/CompanyController.php
+++ app/Http/Controllers/Api/CompanyController.php
@@ -2,49 +2,51 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
 use App\Models\Company;
-use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CompanyRequest;
+use App\Http\Resources\CompanyResource;
 
-class CompanyController extends Controller
-{
+class CompanyController extends Controller {
     /**
      * Display a listing of the resource.
      */
-    public function index()
-    {
-        //
+    public function index() {
+        return CompanyResource::collection(Company::all());
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
-    {
-        //
+    public function store(CompanyRequest $request) {
+
+        $company = Company::create($request->validated);
+
+        return new CompanyResource($company);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Company $company)
-    {
-        //
+    public function show(Company $company) {
+        return new CompanyResource($company);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Company $company)
-    {
-        //
+    public function update(CompanyRequest $request, Company $company) {
+        $company->update($request->validated);
+
+        return new CompanyResource($company);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Company $company)
-    {
-        //
+    public function destroy(Company $company) {
+        $company->delete();
+
+        return  response()->noContent();
     }
 }
